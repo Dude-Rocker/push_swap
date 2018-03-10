dc4fa1fb237894a5d1a3be369991f0f5438f25b8
Vyacheslav GLADUSH <vgladush@e2r3p3.unit.ua>
2018-03-10 17:37:45 +0200

    bgcr

--- srcs/ft_operations.c
+++ srcs/ft_operations.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_operations.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vgladush <vgladush@student.unit.ua>        +#+  +:+       +#+        */
+/*   By: vgladush <vgladush@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 16:57:00 by vgladush          #+#    #+#             */
-/*   Updated: 2018/03/10 17:09:21 by vgladush         ###   ########.fr       */
+/*   Updated: 2018/03/10 17:37:22 by vgladush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,25 @@
 void		ft_operations(t_stack *dig, int i)
 {
 	if (i == 1)
-		swap_sa(dig);
+		swap_ss(dig, 1);
 	else if (i == 2)
-		swap_sb(dig);
+		swap_ss(dig, 2);
 	else if (i == 3)
-		swap_ss(dig);
+		swap_ss(dig, 3);
 	else if (i == 4)
-		swap_pa(dig);
+		swap_pp(dig, 1);
 	else if (i == 5)
-		swap_pb(dig);
+		swap_pp(dig, 2);
 	else if (i == 6)
-		swap_ra(dig);
+		swap_rr(dig, 1);
 	else if (i == 7)
-		swap_rb(dig);
+		swap_rr(dig, 2);
 	else if (i == 8)
-		swap_rr(dig);
+		swap_rr(dig, 3);
 	else if (i == 9)
-		swap_rra(dig);
+		swap_rrr(dig, 1);
 	else if (i == 10)
-		swap_rrb(dig);
+		swap_rrr(dig, 2);
 	else if (i == 11)
-		swap_rrr(dig);
+		swap_rrr(dig, 3);
 }
